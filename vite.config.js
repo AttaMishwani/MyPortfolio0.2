ea9012fdff2b697783663067951767e2135b1fd5
Atta Ur Rehman <145256710+Atta2006@users.noreply.github.com>
2024-11-21 14:38:40 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   plugins: [react(), ghPages()],
+  server: {
+    port: 3000,  // Change to any other available port
+  },
   base: '/MyPortfolio0.2/', // Use the repository name here
 });
