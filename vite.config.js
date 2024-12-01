2c53b12cf453385a7bb1947ee87467bf93bb31ab
Atta Ur Rehman <145256710+Atta2006@users.noreply.github.com>
2024-12-01 20:53:56 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
 
+// GitHub Pages config
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
+  base: '/MyPortfolio0.2/',  // Repository name
   server: {
-    port: 3000,  // Change to any other available port
-  },
-  base: '/MyPortfolio0.2/', // Use the repository name here
+    port: 3000,  // Optional: Change if needed
+  }
 });
